52567726f800e46a5b0b204eba8fbe00e838ea84
GabArroM <gaboarma1@gmail.com>
2024-06-11 13:59:31 -0600

    Inicializar repositorio

---
+++ src/main/java/com/OceanoVivo/OceanoVivoApplication.java
@@ -0,0 +1,13 @@
+package com.OceanoVivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanoVivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanoVivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OceanoVivo/OceanoVivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OceanoVivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanoVivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
